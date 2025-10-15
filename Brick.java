275bc59cfc8e8e7c32a03212f3acc79f5a4debaa
LTD07072006 <tuandung1232006@gmail.com>
2025-10-15 22:26:37 +0700

    Update Brick.java

--- Brick.java
+++ Brick.java
@@ -1,62 +1,24 @@
-package game;
+package org.example.akarnoidgame;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class Brick extends GameObject {
-    private int health;
-    private boolean destroyed = false;
-
-    public Brick(double x, double y, double width, double height) {
-        super(x, y, width, height, null);
-        this.health = 1;
-    }
-
     public Brick(double x, double y, double width, double height, String imagePath) {
         super(x, y, width, height, imagePath);
-        this.health = 1;
     }
 
     @Override
-    public void update() {
-    }
+    public void update() {}
 
     @Override
     public void render(GraphicsContext gc) {
         if (!visible) return;
-
-        if (img != null) {
+        if (img != null)
             gc.drawImage(img, x, y, width, height);
-        } else {
-            switch (health) {
-                case 3 -> gc.setFill(Color.DARKRED);
-                case 2 -> gc.setFill(Color.ORANGE);
-                default -> gc.setFill(Color.YELLOW);
-            }
+        else {
+            gc.setFill(Color.ORANGE);
             gc.fillRect(x, y, width, height);
-            gc.setStroke(Color.BLACK);
-            gc.strokeRect(x, y, width, height);
         }
-    }
-
-    public void hit() {
-        if (destroyed) return;
-        health--;
-        if (health <= 0) {
-            destroyed = true;
-            visible = false;
-        }
-    }
-
-    public boolean isDestroyed() {
-        return destroyed;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void setHealth(int h) {
-        this.health = h;
     }
 }
