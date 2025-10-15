5d40f6153e87affda9e7181941649489ac20b79c
LTD07072006 <tuandung1232006@gmail.com>
2025-10-15 22:27:17 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,12 @@
-package game;
+package org.example.akarnoidgame;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    private static final double WIDTH = 800;
-    private static final double HEIGHT = 600;
-
     @Override
     public void start(Stage stage) {
-        GameCanvas gameCanvas = new GameCanvas(WIDTH, HEIGHT);
-        Scene scene = new Scene(new javafx.scene.Group(gameCanvas));
-
-        stage.setTitle("Arkanoid - Group 4");
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
-
-        // Đặt focus cho canvas để nhận input
-        gameCanvas.requestFocus();
+        new GameManager(stage);
     }
 
     public static void main(String[] args) {
