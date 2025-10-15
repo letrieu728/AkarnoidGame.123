b71da6ce6fa58859ebce49e55f088e8bf3f3a012
letrieu728 <24022750@vnu.edu.vn>
2025-10-15 22:17:51 +0700

    Update GameObject.java

--- GameObject.java
+++ GameObject.java
@@ -3,43 +3,36 @@ package game;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
+
 public abstract class GameObject {
-    protected double x, y;
-    protected double width, height;
-    protected Image img;
+    protected double x, y, width, height;
     protected boolean visible = true;
+    protected Image img;
 
     public GameObject(double x, double y, double width, double height, String imagePath) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-
         if (imagePath != null) {
-            try {
-                img = new Image(getClass().getResourceAsStream(imagePath));
-            } catch (Exception e) {
-                System.out.println("Không thể tải ảnh: " + imagePath);
-            }
+            img = new Image(imagePath);
         }
     }
 
-    public abstract void render(GraphicsContext gc);
     public abstract void update();
+    public abstract void render(GraphicsContext gc);
 
     public boolean intersects(GameObject other) {
-        return visible && other.visible &&
-                x < other.x + other.width &&
-                x + width > other.x &&
-                y < other.y + other.height &&
-                y + height > other.y;
+        return x < other.x + other.width && x + width > other.x
+                && y < other.y + other.height && y + height > other.y;
     }
 
+    // Getter & Setter
     public double getX() { return x; }
     public double getY() { return y; }
+    public void setY(double y) { this.y = y; }
     public double getWidth() { return width; }
     public double getHeight() { return height; }
-
-    public void setVisible(boolean v) { this.visible = v; }
     public boolean isVisible() { return visible; }
+    public void setVisible(boolean visible) { this.visible = visible; }
 }
