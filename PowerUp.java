c94ab8c47850a9f1cd8eeb1cfd1e3743ce1e3633
LTD07072006 <tuandung1232006@gmail.com>
2025-11-04 21:13:23 +0700

    Update PowerUp.java

--- PowerUp.java
+++ PowerUp.java
@@ -1,21 +1,18 @@
 package org.example.akarnoidgame;
 import javafx.scene.canvas.GraphicsContext;
-/*
- * LỚP TRỪU TƯỢNG MỚI cho tất cả các vật phẩm Power-up.
- * Kế thừa từ MovableObject để có thể di chuyển.
- */
+// Lớp trừu tượng cho tất cả các vật phẩm Power-up.
+
 public abstract class PowerUp extends MovableObject {
 
     public PowerUp(double x, double y, double size, String imagePath) {
         super(x, y, size, size, imagePath);
-        // Tất cả power-up đều rơi xuống với tốc độ như nhau
         this.dy = 2.0;
         this.dx = 0;
     }
 
     @Override
     public void update() {
-        move(); // Di chuyển vật phẩm xuống dưới
+        move();
     }
 
     @Override
@@ -26,4 +23,5 @@ public abstract class PowerUp extends MovableObject {
     }
     public abstract void applyEffect(GameCanvas gameCanvas);
 }
+
 
