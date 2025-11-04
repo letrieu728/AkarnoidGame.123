ef03c1288432051e5a35e4d2ffdde80c042b5d7c
Quang-Huy0804 <thachuy0804@gmail.com>
2025-11-04 20:29:30 +0700

    Update PaddleExpandPowerUp.java

--- PaddleExpandPowerUp.java
+++ PaddleExpandPowerUp.java
@@ -5,7 +5,7 @@ public class PaddleExpandPowerUp extends PowerUp {
     public PaddleExpandPowerUp(double x, double y) {
         super(x, y, 40, "/image/expand.png");
     }
-
+    
     @Override
     public void applyEffect(GameCanvas gameCanvas) {
         gameCanvas.getPaddle().expand();
