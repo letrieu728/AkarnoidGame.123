c0e5d6dd679d667d331633e3dc06113d5e87e6e8
LTD07072006 <tuandung1232006@gmail.com>
2025-11-04 21:15:07 +0700

    Update BulletPowerUp.java

--- BulletPowerUp.java
+++ BulletPowerUp.java
@@ -3,13 +3,12 @@ package org.example.akarnoidgame;
 public class BulletPowerUp extends PowerUp {
 
     public BulletPowerUp(double x, double y) {
-        // Gọi constructor của lớp cha với đường dẫn ảnh tương ứng
         super(x, y, 40, "/image/bullet.png");
     }
 
     @Override
     public void applyEffect(GameCanvas gameCanvas) {
-        // Hiệu ứng của vật phẩm này là gọi hàm bắn đạn trong GameCanvas
         gameCanvas.shootBullets();
     }
+
 }
