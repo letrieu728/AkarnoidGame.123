c31659458885044ec349ea568ffd65cc00be1fb9
kimanh15 <24022614@vnu.edu.vn>
2025-11-05 02:45:13 +0700

    Update x2BallPowerUp

--- X2BallPowerUp.java
+++ X2BallPowerUp.java
@@ -1,5 +1,8 @@
 package org.example.akarnoidgame;
 
+/**
+ * Vật phẩm "x2 bóng" - khi nhặt được sẽ x2 bóng trong game.
+ */
 public class X2BallPowerUp extends PowerUp {
 
     public X2BallPowerUp(double x, double y) {
