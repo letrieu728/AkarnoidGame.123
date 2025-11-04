53ed9c434f390168bf5e4e986882847c5aa00251
LTD07072006 <tuandung1232006@gmail.com>
2025-11-04 21:15:36 +0700

    Update Bullet.java

--- Bullet.java
+++ Bullet.java
@@ -5,18 +5,14 @@ import javafx.scene.canvas.GraphicsContext;
 public class Bullet extends MovableObject {
 
     public Bullet(double startX, double startY) {
-        // Khởi tạo viên đạn với kích thước 10x20 pixels
-        // startX được truyền vào là tâm của paddle, ta trừ đi 5 để viên đạn nằm chính giữa
-        super(startX - 5, startY, 10, 20, "/image/bullet.png");
-
-        // Thiết lập tốc độ bay thẳng lên trên
+        super(startX - 5, startY, 10, 20, "/image/bullet.png");n
         this.dy = -8.0; // Giá trị âm để di chuyển lên
         this.dx = 0;
     }
 
     @Override
     public void update() {
-        move(); // Di chuyển viên đạn theo vận tốc dy
+        move();
     }
 
     @Override
@@ -26,3 +22,4 @@ public class Bullet extends MovableObject {
         }
     }
 }
+
