4708475c70ef93b17e59becfad2c1e1f04ec6b37
kimanh15 <24022614@vnu.edu.vn>
2025-11-12 00:28:53 +0700

    Update

---
+++ GameManager.java
@@ -0,0 +1,27 @@
+package org.example.akarnoidgame;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Lớp quản lý màn chơi, có thể mở rộng thêm menu, restart.
+ */
+public class GameManager {
+    private final Stage stage;
+    private Scene gameScene;
+    private GameCanvas gameCanvas;
+
+    public GameManager(Stage stage) {
+        this.stage = stage;
+        initGame();
+    }
+
+    private void initGame() {
+        gameCanvas = new GameCanvas(900, 600);
+        gameScene = new Scene(gameCanvas);
+        stage.setTitle("Arkanoid ");
+        stage.setScene(gameScene);
+        stage.show();
+        gameCanvas.requestFocus();
+    }
+}
